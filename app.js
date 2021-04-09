95ef4abdb82a417de8cf678cf09069f74118efb2
Allan Silva <allansilva956@gmail.com>
2021-04-08 23:33:49 -0400

    Render a frontend project from codepen on main route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Word!!!!');
+    res.sendFile(path.join(__dirname + '/views/3dCssPlane/index.html'));
 });
 
 app.listen(PORT, HOST);
